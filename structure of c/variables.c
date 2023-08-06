aa65c4e2c61ec44ceb51cbd86d7ddfb364df32ac
ankit <ankitkuswaha07@gmail.com>
2023-08-06 21:07:52 +0530

    variables in c

---
+++ structure of c/variables.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    /*variables declaration*/
+    int b,result;
+
+
+    /*variables declaration + variable definition on one line*/
+    int a=2;
+
+
+
+    /*variable definition*/
+    b=3;
+
+    
+  result = a + b;
+    printf("%d = %d + %d ",result,a,b);
+    return 0;
+
+
+
+}
