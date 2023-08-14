8b86f010710586bc66c69da381725fe62a5c9445
ankit <ankitkuswaha07@gmail.com>
2023-08-14 23:25:55 +0530

    constant in c

---
+++ structure of c/constants in c.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#define value 40000
+
+int main()
+{
+    
+
+    // value = 690; error
+    printf("%d",value);
+
+
+
+    const int a = 78;
+    // a = 76; error
+    printf("%d",a);
+
+
+  return 0;
+}
