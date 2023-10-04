acad4c953951812d6de9c275f5ea71140518b43f
ankit <ankitkuswaha07@gmail.com>
2023-10-04 15:55:57 +0530

    Pointer Types: Null Pointer, Wide Pointer, Void Pointer

---
+++ 09 derived datatypes/08 Pointer Types Null Pointer&Wide Pointer&Void Pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+//     // NULL POINTER
+//     int *ptr = NULL;
+//     printf("\n the value of the null pointer is %d",ptr);
+
+// // VOID POINTER
+//     void *p = NULL;
+//     printf("\n the size of a void pointer is %d",sizeof(p));
+
+    // WILD POINTER
+     int *w;
+    printf("\n the size of a void pointer is %d",*w);
+    return 0;
+}
