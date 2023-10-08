2e7991fcfa783316438ae1cc0768d51168891672
ankit <ankitkuswaha07@gmail.com>
2023-10-08 14:50:45 +0530

    Overview of File Handling

---
+++ 09 File Handling in C/01 Overview of File Handling.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int c;
+
+
+// getchat and putchar funtiuon
+    // printf("/n enter a character");
+    // c = getchar ( );
+
+    // printf("\nYour Character ");
+    // putchar( c );
+
+
+
+    // gets and puts funtion
+    char ch[100];
+    printf("\n Enter a text : ");
+    gets(ch);
+    printf("\n YOu entered : ");
+    puts(ch);
+    return 0;
+}
