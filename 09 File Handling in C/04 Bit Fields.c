fcf579b665ec4a41ea4fdf75e0b3612a362b21fa
ankit <ankitkuswaha07@gmail.com>
2023-10-09 19:39:40 +0530

    Bit Fields

---
+++ 09 File Handling in C/04 Bit Fields.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// define simple structure
+struct
+{
+    unsigned int a;
+    unsigned int b;
+}e1;
+
+
+// define simple structure with bits feilds
+struct 
+{
+    unsigned int a : 1;
+    unsigned int b : 1;
+
+}e2;
+
+int main(){
+    printf("\n the sixe of e1 is %d",sizeof(e1));
+    printf("\n the sixe of e2 is %d",sizeof(e2));
+    return 0;
+}
