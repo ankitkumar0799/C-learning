99a5467bde6243543408e514adc7b9254a8a3a1c
ankit <ankitkuswaha07@gmail.com>
2023-10-11 18:22:03 +0530

    Assignment : Preprocessors and Header Files

---
+++ 10 Preprocessors and Header Files/02 Assignment   Preprocessors and Header Files.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    //1. what is the functionality of #include directive
+    //2. list down various header files used in c programming
+    // <stdio.h>
+    // <stdlib.h>
+    return 0;
+}
