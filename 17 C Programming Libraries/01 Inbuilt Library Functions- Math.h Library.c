45a1fc0fe4ae9d8fe4b3251ea57c4bb5389e26f3
ankit <ankitkuswaha07@gmail.com>
2023-10-19 00:57:11 +0530

    Inbuilt Library Functions- Math.h Library

---
+++ 17 C Programming Libraries/01 Inbuilt Library Functions- Math.h Library.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+
+    // squre root funtion sqrt()
+    // double number;
+    // printf("\n enter your number");
+    // scanf("%lf", &number);
+    // double sqaureroot = sqrt(number);
+
+    // printf("\n the number is %lf", sqaureroot);
+
+
+
+//power funtion pow()
+// double base,power,result;
+
+// printf("\n enter your base number");
+// scanf("%lf",&base);
+
+// printf("\n enter your  power");
+// scanf("%lf",&power);
+
+// result = pow(base,power);
+
+// printf("\n %lf^%lf = %lf",base,power,result);
+
+
+
+// /log funtion
+// double num = 5.6, result;
+
+// result = log(num);
+// printf("log(%.lf) = %.2f",num, result);
+
+
+
+
+
+// sin() funtion
+// double x = 2.3;
+// double result = sin(x);
+// printf("sun(%.2lf) = %.2lf",x,result);
+
+
+// cos() funtion
+// double x = 2.3;
+// double result = cos(x);
+// printf("sun(%.2lf) = %.2lf",x,result);
+
+
+
+
+    return 0;
+}
