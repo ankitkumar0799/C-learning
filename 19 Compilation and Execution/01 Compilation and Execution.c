6e41da658d72f651b02833a0a7385fd7b617067d
ankit <ankitkuswaha07@gmail.com>
2023-10-20 00:26:25 +0530

    Compilation and Execution

---
+++ 19 Compilation and Execution/01 Compilation and Execution.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    // 1 .preprocesser
+    // it takes the sorce code as an input and removes all the comments from the sorce code
+
+
+    // 2. compiler
+    // complier checks the any errror in the source code then compiler converts code provided by the preprocessor into assembly code.
+
+
+    // 3. assembler
+    // it help to convert the assembly code to machine level code
+
+
+
+    // 4. linker
+    // it is used to link the object code included libareies and object of our programs
+    return 0;
+}
